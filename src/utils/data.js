da9be4be612f512da83a42196b22063206d95a35
unknown <mustaphas902@gmail.com>
2025-10-30 10:22:38 +0100

    logo

--- src/utils/data.js
+++ src/utils/data.js
@@ -1,35 +1,14 @@
 export const CARD_BG = [
-    {
-        id: 1, bgColor: "linear-gradient(135deg, #e6f8f3 0%, #f7fcfa 100%)"
-    },
-    {
-        id: 2, bgColor: "linear-gradient(135deg, #e6f8f3 0%, #f7fcfa 100%)"
-    },
-    {
-        id: 3, bgColor: "linear-gradient(135deg, #e6f8f3 0%, #f7fcfa 100%)"
-    },
-    {
-        id: 4, bgColor: "linear-gradient(135deg, #e6f8f3 0%, #f7fcfa 100%)"
-    },
-    {
-        id: 5, bgColor: "linear-gradient(135deg, #e6f8f3 0%, #f7fcfa 100%)"
-    },
-    {
-        id: 6, bgColor: "linear-gradient(135deg, #e6f8f3 0%, #f7fcfa 100%)"
-    },
-    {
-        id: 7, bgColor: "linear-gradient(135deg, #e6f8f3 0%, #f7fcfa 100%)"
-    },
-    {
-        id: 8, bgColor: "linear-gradient(135deg, #e6f8f3 0%, #f7fcfa 100%)"
-    },
-    {
-        id: 9, bgColor: "linear-gradient(135deg, #e6f8f3 0%, #f7fcfa 100%)"
-    },
-    {
-        id: 10, bgColor: "linear-gradient(135deg, #e6f8f3 0%, #f7fcfa 100%)"
-    },
-
+    { id: 1, bgColor: "linear-gradient(135deg, #e0f2fe 0%, #bfdbfe 100%)" },
+    { id: 2, bgColor: "linear-gradient(135deg, #ede9fe 0%, #f5d0fe 100%)" },
+    { id: 3, bgColor: "linear-gradient(135deg, #ecfccb 0%, #bbf7d0 100%)" },
+    { id: 4, bgColor: "linear-gradient(135deg, #fff7ed 0%, #ffedd5 100%)" },
+    { id: 5, bgColor: "linear-gradient(135deg, #f0f9ff 0%, #e0f2fe 100%)" },
+    { id: 6, bgColor: "linear-gradient(135deg, #ffe4e6 0%, #ffd7e2 100%)" },
+    { id: 7, bgColor: "linear-gradient(135deg, #e6fffb 0%, #bbf7d0 100%)" },
+    { id: 8, bgColor: "linear-gradient(135deg, #eef2ff 0%, #e9d5ff 100%)" },
+    { id: 9, bgColor: "linear-gradient(135deg, #fff9db 0%, #fff1a8 100%)" },
+    { id: 10, bgColor: "linear-gradient(135deg, #fef3c7 0%, #fde68a 100%)" }
 ]
 
 
